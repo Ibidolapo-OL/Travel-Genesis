46c3937fca43e896b84050e1c0a2aad19d13e242
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-12-10 15:51:21 +0100

    new

--- Onboarding 2/onboarding2.js
+++ Onboarding 2/onboarding2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
   const getStartedButton = document.getElementById("getStartedBtn");
   if (getStartedButton) {
@@ -13,20 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error('Error:Button with ID "getStartedBtn" not found.');
   }
 });
-=======
-document.addEventListener('DOMContentLoaded', function(){
-     
-     const getStartedButton = document.getElementsByid('getStartedBtn');
-     if (getStartedButton) {
-          getStartedButton.addEventListener('click', function() {
-               console.log('Get Started button clicked! Navigating to Sign Up page...');
-               
-          //window.location.href = 'signup.html';
-          
-          alert('Proceeding to the next screen (Sign Up).');
-          });
-     } else {
-          console.error('Error:Button with ID "getStartedBtn" not found.');
-     }
+document.addEventListener("DOMContentLoaded", function () {
+  const getStartedButton = document.getElementsByid("getStartedBtn");
+  if (getStartedButton) {
+    getStartedButton.addEventListener("click", function () {
+      console.log("Get Started button clicked! Navigating to Sign Up page...");
+
+      //window.location.href = 'signup.html';
+
+      alert("Proceeding to the next screen (Sign Up).");
+    });
+  } else {
+    console.error('Error:Button with ID "getStartedBtn" not found.');
+  }
 });
->>>>>>> 4304cac9346463e70d3cb616432572bc25222751
