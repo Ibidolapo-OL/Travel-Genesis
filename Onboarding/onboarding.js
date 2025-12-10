64cfdd2d867afceee2fde1b62b07b9d0e5fe2a18
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-12-10 14:15:06 +0100

    js added to to the first four pages

--- Onboarding/onboarding.js
+++ Onboarding/onboarding.js
@@ -5,3 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "../Onboarding 2/onboarding2.html";
   });
 });
+// Skip button functionality
+document.addEventListener("DOMContentLoaded", () => {
+  const skipButton = document.getElementById("skipBtn");
+
+  skipButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "../Signup Screen/signup.html";
+  });
+});
