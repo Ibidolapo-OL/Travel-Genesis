578570db950c5ff1914cfe281f7e5251f7b7938e
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-12-12 14:53:55 +0100

    stew

--- Bookingdetails/bookings.js
+++ Bookingdetails/bookings.js
@@ -3,16 +3,42 @@ document.querySelectorAll(".edit-btn").forEach((btn) => {
   btn.addEventListener("click", function () {
     const item = this.closest(".detail-item");
     const label = item.querySelector(".detail-label").textContent;
-    alert(`Edit ${label}`);
+    // alert(`Edit ${label}`);
   });
 });
 
 // Back button functionality
 document.querySelector(".back-btn").addEventListener("click", function () {
-  alert("Go back to previous page");
+  // alert("Go back to previous page");
 });
 
 // Continue button functionality
 document.querySelector(".continue-btn").addEventListener("click", function () {
-  alert("Proceeding to payment...");
+  // alert("Proceeding to payment...");
+});
+
+///when the booking back is clicked
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bookingbackbtn = document.getElementById("back-btn");
+
+  // Back button - navigate to previous page
+  if (bookingbackbtn) {
+    bookingbackbtn.addEventListener("click", () => {
+      window.location.href = "../SEAT/seat.html";
+    });
+  }
+});
+
+///when the continue is beeng is clicked
+
+document.addEventListener("DOMContentLoaded", () => {
+  const continuebtn = document.getElementById("continue-btn");
+
+  // Back button - navigate to previous page
+  if (continuebtn) {
+    continuebtn.addEventListener("click", () => {
+      window.location.href = "../Payment/payment.html";
+    });
+  }
 });
--- SEAT/seat.js
+++ SEAT/seat.js
@@ -7,3 +7,40 @@ document.querySelectorAll(".seat.available, .seat.selected").forEach((seat) => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const backBtn = document.getElementById("back-btn");
+
+  // Back button - navigate to previous page
+  if (backBtn) {
+    backBtn.addEventListener("click", () => {
+      window.location.href = "../DatePicker/datepicker.html";
+    });
+  }
+});
+
+///when the continue is clicked
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contineuBtn = document.getElementById("continue-btn");
+
+  // Back button - navigate to previous page
+  if (contineuBtn) {
+    contineuBtn.addEventListener("click", () => {
+      window.location.href = "../Bookingdetails/bookings.html";
+    });
+  }
+});
+
+///when the continue back is clicked
+
+document.addEventListener("DOMContentLoaded", () => {
+  const continebackBtn = document.getElementById("back-btn");
+
+  // Back button - navigate to previous page
+  if (continebackBtn) {
+    continebackBtn.addEventListener("click", () => {
+      window.location.href = "../DatePicker/datepicker.html";
+    });
+  }
+});
