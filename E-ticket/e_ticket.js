129e4ba446ee54eec24ccd4b6d53b4b84f3dd319
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2025-12-12 16:00:37 +0100

    done

---
+++ E-ticket/e_ticket.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const retrunhome = document.getElementById("return-button");
+  if (retrunhome) {
+    retrunhome.addEventListener("click", function () {
+      console.log("returning to home page...");
+
+      window.location.href = "../Home-screen/home.html";
+    });
+  }
+});
